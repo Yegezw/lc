0ab4b16eca2b9dd27cc14aa319764ce68eb4a9cd
ld <zzw422339@163.com>
2023-12-29 15:01:31 +0800

    螺旋矩阵题目

---
+++ src/main/java/p2_arr/Solution3.java
@@ -0,0 +1,75 @@
+package p2_arr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("all")
+public class Solution3 {
+
+    /**
+     * <a href="https://leetcode.cn/problems/spiral-matrix/description/">54. 螺旋矩阵</a>
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        // l   r
+        // 1 2 3 t
+        // 4 5 6
+        // 7 8 9 b
+        int m = matrix.length;    // 行
+        int n = matrix[0].length; // 列
+        int count = m * n;
+
+        int left = 0, right = n - 1;
+        int top = 0, bottom = m - 1;
+
+        List<Integer> res = new ArrayList<>();
+        while (true) {
+            if (res.size() == count) break;
+            for (int i = left; i <= right; i++) res.add(matrix[top][i]);    // 向右遍历
+            top++;
+
+            if (res.size() == count) break;
+            for (int i = top; i <= bottom; i++) res.add(matrix[i][right]);  // 向下遍历
+            right--;
+
+            if (res.size() == count) break;
+            for (int i = right; i >= left; i--) res.add(matrix[bottom][i]); // 向左遍历
+            bottom--;
+
+            if (res.size() == count) break;
+            for (int i = bottom; i >= top; i--) res.add(matrix[i][left]);   // 向上遍历
+            left++;
+        }
+
+        return res;
+    }
+
+    /**
+     * <a href="https://leetcode.cn/problems/spiral-matrix-ii/description/">59. 螺旋矩阵 II</a>
+     */
+    public int[][] generateMatrix(int n) {
+        // l   r
+        // 1 2 3 t
+        // 4 5 6
+        // 7 8 9 b
+        int[][] arr = new int[n][n];
+        int num = 1;
+        int left = 0, right = n - 1;
+        int top = 0, bottom = n - 1;
+
+        while (num <= n * n) {
+            for (int i = left; i <= right; i++) arr[top][i] = num++;    // 向右填充
+            top++;
+
+            for (int i = top; i <= bottom; i++) arr[i][right] = num++;  // 向下填充
+            right--;
+
+            for (int i = right; i >= left; i--) arr[bottom][i] = num++; // 向左填充
+            bottom--;
+
+            for (int i = bottom; i >= top; i--) arr[i][left] = num++;   // 向上填充
+            left++;
+        }
+
+        return arr;
+    }
+}
