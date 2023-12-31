b0aa3b10737822e2372a02eed5a8e6150341f911
ld <zzw422339@163.com>
2023-12-31 14:36:47 +0800

    补充二分查找题目

--- src/main/java/p2_arr/binary/Solution1.java
+++ src/main/java/p2_arr/binary/Solution1.java
@@ -24,6 +24,43 @@ public class Solution1 {
     }
 
     /**
+     * <a href="https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/description/">34. 在排序数组中查找元素的第一个和最后一个位置</a>
+     */
+    public int[] searchRange(int[] nums, int target) {
+        int l = searchL(nums, target);
+        int r = searchR(nums, target);
+        return new int[]{l, r};
+    }
+
+    private int searchL(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length;
+        int mid;
+
+        while (l < r) {
+            mid = l + (r - l) / 2;
+            if (nums[mid] >= target) r = mid;
+            else l = mid + 1;
+        }
+
+        return r != nums.length && nums[r] == target ? r : -1;
+    }
+
+    private int searchR(int[] nums, int target) {
+        int l = -1;
+        int r = nums.length - 1;
+        int mid;
+
+        while (l < r) {
+            mid = l + (r - l + 1) / 2;
+            if (nums[mid] <= target) l = mid;
+            else r = mid - 1;
+        }
+
+        return l != -1 && nums[l] == target ? l : -1;
+    }
+
+    /**
      * <a href="https://leetcode.cn/problems/koko-eating-bananas/description/">875. 爱吃香蕉的珂珂</a>
      */
     public int minEatingSpeed(int[] piles, int h) {
