9854048f681cc8d51fc830eac97e3be53ccac068
ld <zzw422339@163.com>
2024-01-03 17:39:45 +0800

    优化螺旋矩阵题目解法

--- src/main/java/p2_arr/Solution3.java
+++ src/main/java/p2_arr/Solution3.java
@@ -56,16 +56,20 @@ public class Solution3 {
         int left = 0, right = n - 1;
         int top = 0, bottom = n - 1;
 
-        while (num <= n * n) {
+        while (true) {
+            if (num > n * n) break;
             for (int i = left; i <= right; i++) arr[top][i] = num++;    // 向右填充
             top++;
 
+            if (num > n * n) break;
             for (int i = top; i <= bottom; i++) arr[i][right] = num++;  // 向下填充
             right--;
 
+            if (num > n * n) break;
             for (int i = right; i >= left; i--) arr[bottom][i] = num++; // 向左填充
             bottom--;
 
+            if (num > n * n) break;
             for (int i = bottom; i >= top; i--) arr[i][left] = num++;   // 向上填充
             left++;
         }
